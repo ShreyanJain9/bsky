6de507fb43e977d1c0b213e39c251f1066b2d87c
Shreyan Jain <67488567+ShreyanJain9@users.noreply.github.com>
2023-04-09 13:59:14 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import atprototools as atpt
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/post", methods=["POST"])
+def post_skoot():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    post_content = request.form.get("post-content")
+    atpt.login(username, password)
+    atpt.post_skoot(post_content)
+    return "Posted to BlueSky successfully!"
+
+if __name__ == "__main__":
+    app.run
